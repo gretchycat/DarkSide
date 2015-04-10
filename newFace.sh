3cef3412af52a22a126fb9da3743a8c8a9f63703
Gretchen Maculo <iPocketCast@google.com>
2015-04-10 12:53:28 -0600

    adding TheEndlessRiver watchface

--- newFace.sh
+++ newFace.sh
@@ -5,15 +5,16 @@ cd ../images
 cp -av DarkSide $1
 cd ../..
 cat appinfo.template.json|sed -e "s/FACENAME/$1/g">appinfo.${1}.json
-ln -sf appinfo.${1}.json
+ln -sf appinfo.${1}.json appinfo.json
 convert ~/Downloads/$1-icon.png -dither FloydSteinberg -remap resources/images/6bit.png resources/images/$1/color/darkside-icon.png
 convert ~/Downloads/${1}.png -dither FloydSteinberg -remap resources/images/6bit.png resources/images/$1/color/darkside.png
-convert ~/Downloads/$1-icon.png -dither FloydSteinberg -monochrome resources/images/$1/darkside-icon.png
-convert ~/Downloads/${1}.png -dither FloydSteinberg -monochrome resources/images/$1/darkside.png
+convert ~/Downloads/$1-icon.png -dither FloydSteinberg -brightness-contrast=+10 -monochrome resources/images/$1/darkside-icon.png
+convert ~/Downloads/${1}.png -dither FloydSteinberg -brightness-contrast=+10 -monochrome resources/images/$1/darkside.png
 pebble clean
 pebble build
 git add resources/fonts/$1
 git add respource/images/$1
 git add appinfo.${1}.json
 git commit . -m "adding $1 watchface"
+pebble install --emulator basalt
 
