63062d74ba76f9634cab13868b9b33b06a7702e1
Gretchen Maculo <iPocketCast@google.com>
2015-06-06 17:33:28 -0600

    fix bug

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+rm *.pbw
 for i in appinfo.[A-Z]*.json
 do
 	FACE=`echo $i|cut -f2 -d'.'`
+	ln -sf $i appinfo.json
 	pebble clean &&pebble build &&cp build/Darkside.pbw $FACE.pbw
 done
 
