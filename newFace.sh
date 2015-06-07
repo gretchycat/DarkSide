9831876f775b7dbef6ec6ee4451e7e973eab5929
Gretchen Maculo <iPocketCast@google.com>
2015-06-06 19:35:10 -0600

    adding Jupiter watchface

--- newFace.sh
+++ newFace.sh
@@ -6,10 +6,10 @@ cp -av DarkSide $1
 cd ../..
 cat appinfo.template.json|sed -e "s/FACENAME/$1/g">appinfo.${1}.json
 ln -sf appinfo.${1}.json appinfo.json
-convert ~/Downloads/$1-icon.png -dither FloydSteinberg -remap resources/images/6bit.png resources/images/$1/color/darkside-icon.png
-convert ~/Downloads/${1}.png -dither FloydSteinberg -remap resources/images/6bit.png resources/images/$1/color/darkside.png
-convert ~/Downloads/$1-icon.png -dither FloydSteinberg -brightness-contrast +10 -monochrome resources/images/$1/darkside-icon.png
-convert ~/Downloads/${1}.png -dither FloydSteinberg -contrast +10 -monochrome resources/images/$1/darkside.png
+convert ~/Downloads/$1-icon.png -dither FloydSteinberg -remap resources/images/6bit.png resources/images/$1/color/darkside-icon~color.png
+convert ~/Downloads/${1}.png -dither FloydSteinberg -remap resources/images/6bit.png resources/images/$1/darkside~color.png
+convert ~/Downloads/$1-icon.png -dither FloydSteinberg -brightness-contrast +10 -monochrome resources/images/$1/darkside-icon~bw.png
+convert ~/Downloads/${1}.png -dither FloydSteinberg -contrast +10 -monochrome resources/images/$1/darkside~bw.png
 pebble clean
 pebble build
 git add resources/fonts/$1
